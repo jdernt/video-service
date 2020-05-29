039a5eebcec4349a2780c66d40ebe63d195c26c7
jdernt <jdernt@gmail.com>
2020-05-29 15:37:54 +0400

    changes

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
 
+// сохранение логина
+
+function InitialState(){
+	if (localStorage.getItem('login') == null) {
+		$('.header__login').show();
+	} else {
+		$('.header__login').hide();
+		$('.header__auth').html(localStorage.getItem('login'));
+	}
+};
+
+InitialState();
+
+function addToStorage(){
+	let content = $('.header__auth').html();
+	console.log(content);
+
+	localStorage.setItem('login', content);
+}
+
+// переключение между табами
+
 const latestSection = $('.latest');
 const	genreSection  = $('.genre');
 const channelSection = $('.channel');
@@ -26,6 +48,8 @@ $('.header__films').click(function selectFilms(b){
 
 });
 
+// вызов модального окна
+
 $('.header__login').click(function(){
 	$('.modal').show();
 	$('body').addClass('no__scroll');
@@ -38,6 +62,7 @@ $('.modal').click(function(event){
 	}
 });
 
+// функция авторизации
 
 function auth(c){
 	c.preventDefault();
@@ -46,38 +71,67 @@ function auth(c){
 	let userPassword = $('.modal__input_password').val();
 
 	if (userName.length !== 0 && userPassword.length !== 0) {
-		$('.header__login').hide();
-
-		$('.header__flex').append(`
-		<div class="header__auth">
-			<div class="header__user">${userName}</div>
-			<button class="header__logout">Выйти</button>
-		</div>
-		`);
-
 		$('.modal').hide();
 		$('body').removeClass('no__scroll');
 
+		$('.header__login').hide();
+
+		$('.header__auth').append(`
+			<div class="header__userbar">
+				<div class="header__user">${userName}</div>
+				<button class="header__logout">Выйти</button>
+			</div>
+		`);
+
 		userName = $('.modal__input_login').val('');
 		userPassword = $('.modal__input_password').val('');
+
+		addToStorage();
 	};
 };
 
 $('.modal__login').click(auth);
 
-
-function rename(){
-
-
-};
-
-$('body').on('click', '.header__user', rename);
-
+// вохможность разлогиниться по клику
 
 $('body').on('click', '.header__logout', function(){
-	$('.header__auth').hide();
+	$('.header__userbar').remove();
 	$('.header__login').show();
+
+	localStorage.removeItem('login');
 });
+
+
+// возможность ренейма
+
+$('body').on('click', '.header__user', function(){
+
+	let userNewName;
+
+	$('.header__user').remove();
+	$('.header__userbar').prepend(`
+		<input type="text" class="header__login_input input">
+	`);
+
+	$('header').click(function(event){
+		if (event.target == this) {
+
+		 userNewName = $('.header__login_input').val();
+
+			$('.header__login_input').remove();
+			$('.header__userbar').prepend(`
+				<div class="header__user">${userNewName}</div>
+			`);
+
+			localStorage.removeItem('login');
+			addToStorage();
+
+			userNewName = $('.header__login_input').val('');
+
+		};
+	});
+});
+
 
 
 
